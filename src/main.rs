26d23b656c6e9bd3f1e34b8cbf4c6e6f34da8c0b
Alexandre Pauwels <8890201+ajpauwels@users.noreply.github.com>
2020-05-10 12:31:19 -0400

    Switch to standard warp server start

--- src/main.rs
+++ src/main.rs
@@ -24,47 +24,54 @@ async fn main() {
     let apiv1 = warp::path!("api" / "v1").map(|| "ok\n");
 
     let routes = warp::get().and(healthz.or(apiv1));
-    let filter = warp::service(routes);
-    let make_svc = hyper::service::make_service_fn(move |transport| {
-        // match transport.state {
-        //     State::Handshaking(_) => {
-        //         println!("in handshake");
-        //         future::ok::<_, Infallible>(filter)
-        //     }
-        //     State::Streaming(ref stream) => {
-        //         let (_incoming, sess) = stream.get_ref();
-        //         println!("in streaming, sni = {}", sess.get_sni_hostname().unwrap());
-        //         future::ok::<_, Infallible>(filter)
-        //     }
-        // }
-        // future::ok::<_, Infallible>(filter)
-        // let filter = filter.clone();
-        // match transport.state {
-        //     State::Handshaking(_) => Ok::<_, Infallible>(filter),
-        //     State::Streaming(_) => Ok::<_, Infallible>(filter),
-        // }
-        let inner = filter.clone();
-        let remote_addr = Transport::remote_addr(transport);
-        let peer_certificates = Transport::peer_certificates(transport);
-        future::ok::<_, Infallible>(hyper::service::service_fn(move |req| {
-            inner.call(req, remote_addr)
-        }))
-    });
-
-    let tls_config = TlsConfigBuilder::new()
+    warp::serve(routes)
+        .tls()
         .key_path(tls_key_path)
         .cert_path(tls_cert_path)
         .client_ca_path(tls_client_ca_path)
-        .build()
-        .unwrap();
-    let incoming = AddrIncoming::bind(&(([127, 0, 0, 1], 8080).into()))
-        .and_then(|mut i| {
-            i.set_nodelay(true);
-            Ok(i)
-        })
-        .unwrap();
-    hyper::Server::builder(TlsAcceptor::new(tls_config, incoming))
-        .serve(make_svc)
-        .await
-        .unwrap();
+        .run(([127, 0, 0, 1], 8080))
+        .await;
+    // let filter = warp::service(routes);
+    // let make_svc = hyper::service::make_service_fn(move |transport| {
+    //     match transport.state {
+    //         State::Handshaking(_) => {
+    //             println!("in handshake");
+    //             future::ok::<_, Infallible>(filter)
+    //         }
+    //         State::Streaming(ref stream) => {
+    //             let (_incoming, sess) = stream.get_ref();
+    //             println!("in streaming, sni = {}", sess.get_sni_hostname().unwrap());
+    //             future::ok::<_, Infallible>(filter)
+    //         }
+    //     }
+    //     future::ok::<_, Infallible>(filter)
+    //         let filter = filter.clone();
+    //     match transport.state {
+    //         State::Handshaking(_) => Ok::<_, Infallible>(filter),
+    //         State::Streaming(_) => Ok::<_, Infallible>(filter),
+    //     }
+    //     let inner = filter.clone();
+    //     let remote_addr = Transport::remote_addr(transport);
+    //     let peer_certificates = Transport::peer_certificates(transport);
+    //     future::ok::<_, Infallible>(hyper::service::service_fn(move |req| {
+    //         inner.call(req, remote_addr)
+    //     }))
+    // });
+
+    //     let tls_config = TlsConfigBuilder::new()
+    //         .key_path(tls_key_path)
+    //         .cert_path(tls_cert_path)
+    //         .client_ca_path(tls_client_ca_path)
+    //         .build()
+    //         .unwrap();
+    //     let incoming = AddrIncoming::bind(&(([127, 0, 0, 1], 8080).into()))
+    //         .and_then(|mut i| {
+    //             i.set_nodelay(true);
+    //             Ok(i)
+    //         })
+    //         .unwrap();
+    //     hyper::Server::builder(TlsAcceptor::new(tls_config, incoming))
+    //         .serve(make_svc)
+    //         .await
+    //         .unwrap();
 }
