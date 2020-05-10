cd16660383a850aa8060d629ad6e30dd71f6ba92
Alexandre Pauwels <8890201+ajpauwels@users.noreply.github.com>
2020-05-10 12:33:37 -0400

    Remove unnecessary code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-use futures::future;
-use hyper::server::conn::AddrIncoming;
 use rust_config::Configurator;
-use std::convert::Infallible;
 use warp::Filter;
-
-pub mod tls;
-
-use tls::{TlsAcceptor, TlsConfigBuilder, Transport};
 
 #[tokio::main]
 async fn main() {
@@ -31,47 +24,4 @@ async fn main() {
         .client_ca_path(tls_client_ca_path)
         .run(([127, 0, 0, 1], 8080))
         .await;
-    // let filter = warp::service(routes);
-    // let make_svc = hyper::service::make_service_fn(move |transport| {
-    //     match transport.state {
-    //         State::Handshaking(_) => {
-    //             println!("in handshake");
-    //             future::ok::<_, Infallible>(filter)
-    //         }
-    //         State::Streaming(ref stream) => {
-    //             let (_incoming, sess) = stream.get_ref();
-    //             println!("in streaming, sni = {}", sess.get_sni_hostname().unwrap());
-    //             future::ok::<_, Infallible>(filter)
-    //         }
-    //     }
-    //     future::ok::<_, Infallible>(filter)
-    //         let filter = filter.clone();
-    //     match transport.state {
-    //         State::Handshaking(_) => Ok::<_, Infallible>(filter),
-    //         State::Streaming(_) => Ok::<_, Infallible>(filter),
-    //     }
-    //     let inner = filter.clone();
-    //     let remote_addr = Transport::remote_addr(transport);
-    //     let peer_certificates = Transport::peer_certificates(transport);
-    //     future::ok::<_, Infallible>(hyper::service::service_fn(move |req| {
-    //         inner.call(req, remote_addr)
-    //     }))
-    // });
-
-    //     let tls_config = TlsConfigBuilder::new()
-    //         .key_path(tls_key_path)
-    //         .cert_path(tls_cert_path)
-    //         .client_ca_path(tls_client_ca_path)
-    //         .build()
-    //         .unwrap();
-    //     let incoming = AddrIncoming::bind(&(([127, 0, 0, 1], 8080).into()))
-    //         .and_then(|mut i| {
-    //             i.set_nodelay(true);
-    //             Ok(i)
-    //         })
-    //         .unwrap();
-    //     hyper::Server::builder(TlsAcceptor::new(tls_config, incoming))
-    //         .serve(make_svc)
-    //         .await
-    //         .unwrap();
 }
